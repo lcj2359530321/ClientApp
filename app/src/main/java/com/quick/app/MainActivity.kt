e964164abfc91eb2f0977076c8e48f3613eb373f
lcj <2359530321@qq.com>
2026-03-03 12:22:53 +0800

    2 统一文本管理

--- app/src/main/java/com/quick/app/MainActivity.kt
+++ app/src/main/java/com/quick/app/MainActivity.kt
@@ -1,30 +1,35 @@
 package com.quick.app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.quick.app.ui.theme.ClientAppTheme
+import com.quick.app.core.design.theme.MyAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ClientAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyAppTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                Greeting(
+//                       name = "Android",
+//                      modifier = Modifier.padding(innerPadding)
+//                   )
+//                }
+                MyButton(R.string.agree)
             }
         }
     }
@@ -38,10 +43,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun MyButton(title: Int,modifier: Modifier = Modifier){
+    Button(
+        onClick = { Log.d("MyButton", "MyButton: ")}, modifier = Modifier.fillMaxWidth(), colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+    ) {
+        Text(text = stringResource(id = title))
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ClientAppTheme {
+    MyAppTheme {
         Greeting("Android")
     }
 }
