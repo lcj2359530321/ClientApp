d1264f6be3a42691571a82c084b123634871591b
Dreambutterfly <143479002+lcj2359530321@users.noreply.github.com>
2026-03-14 11:06:16 +0800

    Clean up Config.kt by removing unused constants
    Removed unused constants related to QR code and social media IDs.

--- app/src/main/java/com/quick/app/core/config/Config.kt
+++ app/src/main/java/com/quick/app/core/config/Config.kt
@@ -32,25 +32,4 @@ object Config {
     const val LINK_USER_USER_AGREEMENT = "https://gemini.google.com"
     const val LINK_USER_PRIVACY_POLICY = "https://gemini.google.com/"
 
-
-    /**
-     * 二维码地址
-     * 真实项目中一般设置为应用的下载宣传界面，因为目前没有这样的界面，所以就设置为官网地址
-     */
-    const val QRCODE_URL = "https://www.ixuea.com"
-
-    /**
-     * 用户二维码
-     */
-    val USER_QRCODE_URL = String.format("%s?u=%%s", QRCODE_URL)
-
-    /**
-     * 微信id
-     */
-    const val WECHAT_AK = "wx28456f1c56a1c0c4"
-
-    /**
-     * QQ id
-     */
-    const val QQ_AK = "102081660"
 }
